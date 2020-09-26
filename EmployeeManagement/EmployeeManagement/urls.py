4d764972b831609dc33fd5d429aeb9b972e05564
Prasanth <g.prasanth1000@gmail.com>
2020-09-26 18:43:53 +0530

    admin

--- EmployeeManagement/EmployeeManagement/urls.py
+++ EmployeeManagement/EmployeeManagement/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('admin/',views.admin_dashboard,name='admin_dashboard'),
 ]
--- EmployeeManagement/app/models.py
+++ EmployeeManagement/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class EmployeeDetails(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30,unique=True,primary_key=True)
+    password = models.CharField(max_length=30)
